b5de7e72f70232d30c1eef21af072516650a28ca
n-hoof <211895844+n-hoof@users.noreply.github.com>
2025-12-22 23:54:36 +0100

    Added profiles and esc_user_scores tables. Added supabase auth demo for react front-end as starting point.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,27 @@
-function App(){
+import './App.css'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
+import type { Session } from '@supabase/supabase-js'
+import Auth from './Auth'
+import Account from './Account'
+
+function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <>
-      Hello World!
-    </>
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+    </div>
   )
 }
 
---
+++ supabase/migrations/20251222200933_add_user_scores_table.sql
@@ -0,0 +1,24 @@
+-- Create a table for public profiles
+create table esc_user_scores (
+  id bigint generated always as identity,
+  user_id uuid references public.profiles on delete cascade not null,
+  entry_id bigint references public.esc_entries on delete cascade not null,
+  song_score numeric(3,2) not null check (song_score >= 0.00 and song_score <= 4.00),
+  costume_score numeric(3,2) not null check (costume_score >= 0.00 and costume_score <= 2.00),
+  staging_score numeric(3,2) not null check (staging_score >= 0.00 and staging_score <= 2.00),
+  performance_score numeric(3,2) not null check (performance_score >= 0.00 and performance_score <= 2.00),
+  total numeric(3,2) generated always as (song_score + costume_score + staging_score + performance_score) stored
+);
+
+-- Set up Row Level Security (RLS)
+-- See https://supabase.com/docs/guides/database/postgres/row-level-security for more details.
+alter table esc_user_scores
+  enable row level security;
+create policy "Public scores are viewable by everyone." on esc_user_scores
+  for select using (true);
+create policy "Users can insert scores for their own profile." on esc_user_scores
+  for insert with check ((select auth.uid()) = user_id);
+create policy "Users can update scores for their own profile." on esc_user_scores
+  for update using ((select auth.uid()) = user_id);
+create policy "Users can delete scores for their own profile." on esc_user_scores
+  for delete using ((select auth.uid()) = user_id);
